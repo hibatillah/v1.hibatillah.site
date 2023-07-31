89403001d4c2d5bb7fc6eb3aadde2da1cfb5dad0
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-08-01 03:31:04 +0700

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.join(__dirname, 'src'),
   server: {
     port: 3000,
   }
