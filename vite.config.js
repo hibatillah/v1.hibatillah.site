7d6c53df891fbbd3074c838f07ec3131468aac7b
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-08-01 03:27:04 +0700

    fix: update vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.join(__dirname, 'src'),
   server: {
     port: 3000,
   }
