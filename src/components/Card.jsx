b409026ab148a9b6e98f1ffab6f3d2f906b9812b
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-08-01 05:21:35 +0700

    fix: update card responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,10 +14,10 @@ export const ProjectCard = ({
 }) => {
   return (
     <div className="flex-1 flex gap-16 2xl:gap-20">
-      <div className="relative hidden lg:block flex-none pr-12 w-40">
+      <div className="relative hidden lg:block flex-none pr-12 w-48 2xl:pr-12">
         <p>{date}</p>
         <span className="absolute top-2 -right-px w-2.5 h-2.5 rounded-full bg-main-900 border border-main-400 z-10" />
-        <span className="absolute top-3 right-[3px] w-0.5 lg:h-52 xl:h-56 bg-main-500 z-0" />
+        <span className="absolute top-3 right-[3px] w-0.5 lg:h-52 bg-main-500 z-0 2xl:h-48" />
       </div>
       <Link to={link}>
         <m.div
@@ -29,7 +29,7 @@ export const ProjectCard = ({
             loading="eager"
             className="w-full aspect-video object-cover bg-top rounded-lg bg-main-800 md:w-1/3 md:aspect-[4/3] lg:w-[30%] lg:aspect-[4/3] xl:aspect-video 2xl:w-1/4 2xl:aspect-video pointer-events-none select-none brightness-[85%] group-hover:brightness-90"
           />
-          <div className="md:w-2/3">
+          <div className="md:w-2/3 2xl:w-3/4">
             <p className="text-sm lg:hidden">{date}</p>
             <div className="my-1 flex gap-1.5 items-center">
               <h2 className="line-clamp-1 select-all">{title}</h2>
